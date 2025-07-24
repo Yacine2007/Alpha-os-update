f25e19848ae816a9bc2316b5653bf7a7aed9fbff
B.Y PRO <152791424+Yacine2007@users.noreply.github.com>
2025-07-24 18:46:26 +0100

    Update ex.sh

--- updates/ex.sh
+++ updates/ex.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# فتح الطرفية وعرض الرسالة
-x-terminal-emulator -e bash -c 'echo "The update was successfully installed."; read -n 1 -s -r -p "Press any key to close..."'
+# فتح lxterminal وعرض الرسالة
+if command -v lxterminal >/dev/null 2>&1; then
+    lxterminal -e bash -c 'echo "The update was successfully installed."; read -n 1 -s -r -p "اضغط أي مفتاح لإغلاق التحديث..."'
+else
+    echo "lxterminal غير موجود في النظام."
+fi
